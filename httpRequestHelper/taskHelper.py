b42c1f4ffc0930453cecdd2a59c4a34ccaf2c8f7
Zhe Wang <zhw199@ucsd.edu>
2018-08-29 22:22:01 -0700

    Added some helper funtion created API response class

--- httpRequestHelper/taskHelper.py
+++ httpRequestHelper/taskHelper.py
@@ -1,7 +1,13 @@
 from log import Logger
-
+from tokenGenerator import TokenGenerator
+from exception import Exception
 logger = Logger.getLogger("taskHelper")
+
+
 def setUpTask(location, userToken, radius):
-    logger.info("Location: {}, UserToken: {}, Radius: {} ".format(location, userToken, radius) )
-    token = "fjaosfaosdf"
+    logger.info("Location: {}, UserToken: {}, Radius: {} ".format(location, userToken, radius))
+    if not location:
+        raise Exception.NoLocationError
+    radius = 50 if not radius else radius
+    token = TokenGenerator.generateTaskToken(location, radius)
     return token
